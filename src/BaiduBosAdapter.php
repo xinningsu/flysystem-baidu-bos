8d8d3023b5119bb7752c50267daf9a7a1c7e3259
xinningsu <thomas.su@juwai.com>
2021-03-05 00:05:15 +0800

    some improve

--- src/BaiduBosAdapter.php
+++ src/BaiduBosAdapter.php
@@ -10,6 +10,8 @@ use Sulao\BaiduBos\Client;
 
 class BaiduBosAdapter extends AbstractAdapter
 {
+    use UtilTrait;
+
     /**
      * @var Client
      */
@@ -386,73 +388,6 @@ class BaiduBosAdapter extends AbstractAdapter
     }
 
     /**
-     * Extract options from config
-     *
-     * @param Config $config
-     *
-     * @return array
-     */
-    protected function extractOptions(Config $config)
-    {
-        $options = [];
-
-        foreach (['headers', 'query', 'body', 'request', 'authorize'] as $key) {
-            if ($config->has($key)) {
-                $options[$key] = $config->get($key);
-            }
-        }
-
-        return $options;
-    }
-
-    /**
-     * Normalize the object meta array.
-     *
-     * @param array $meta
-     * @param string $path
-     *
-     * @return array
-     */
-    protected function normalizeMeta(array $meta, $path)
-    {
-        $result =  Util::pathinfo($path);
-
-        if (isset($meta['Last-Modified'])) {
-            $result['timestamp'] = strtotime($meta['Last-Modified']);
-        }
-
-        return array_merge($result, Util::map($meta, [
-            'Content-Length' => 'size',
-            'Content-Type'   => 'mimetype',
-        ]), ['type' => 'file']);
-    }
-
-    /**
-     * Normalize the content from list contents of dir.
-     *
-     * @param array $content
-     *
-     * @return array
-     */
-    protected function normalizeContent(array $content)
-    {
-        $return = [];
-
-        if (substr($content['key'], -1) === '/') {
-            $return['type'] = 'dir';
-            $return['path'] = rtrim($content['key'], '/');
-        } else {
-            $return['type'] = 'file';
-            $return['path'] = $content['key'];
-            $return['size'] = $content['size'];
-        }
-
-        $return['timestamp'] = strtotime($content['lastModified']);
-
-        return $return + Util::pathinfo($content['key']);
-    }
-
-    /**
      * Get object acl, if not set, return bucket acl
      *
      * @param $path
@@ -487,26 +422,5 @@ class BaiduBosAdapter extends AbstractAdapter
         }
 
         return $result['accessControlList'];
-    }
-
-    /**
-     * Extract permissions from acl
-     *
-     * @param array $acl
-     *
-     * @return array
-     */
-    protected function extractPermissions(array $acl)
-    {
-        $permissions = [];
-        foreach ($acl as $row) {
-            $ids = array_column($row['grantee'], 'id');
-            if (in_array('*', $ids)) {
-                $permissions = $row['permission'];
-                break;
-            }
-        }
-
-        return $permissions;
     }
 }
---
+++ src/UtilTrait.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Sulao\Flysystem\BaiduBos;
+
+use League\Flysystem\Config;
+use League\Flysystem\Util;
+
+trait UtilTrait
+{
+    /**
+     * Extract options from config
+     *
+     * @param Config $config
+     *
+     * @return array
+     */
+    protected function extractOptions(Config $config)
+    {
+        $options = [];
+
+        foreach (['headers', 'query', 'body', 'request', 'authorize'] as $key) {
+            if ($config->has($key)) {
+                $options[$key] = $config->get($key);
+            }
+        }
+
+        return $options;
+    }
+
+    /**
+     * Normalize the object meta array.
+     *
+     * @param array $meta
+     * @param string $path
+     *
+     * @return array
+     */
+    protected function normalizeMeta(array $meta, $path)
+    {
+        $result =  Util::pathinfo($path);
+
+        if (isset($meta['Last-Modified'])) {
+            $result['timestamp'] = strtotime($meta['Last-Modified']);
+        }
+
+        return array_merge($result, Util::map($meta, [
+            'Content-Length' => 'size',
+            'Content-Type'   => 'mimetype',
+        ]), ['type' => 'file']);
+    }
+
+    /**
+     * Normalize the content from list contents of dir.
+     *
+     * @param array $content
+     *
+     * @return array
+     */
+    protected function normalizeContent(array $content)
+    {
+        $return = [];
+
+        if (substr($content['key'], -1) === '/') {
+            $return['type'] = 'dir';
+            $return['path'] = rtrim($content['key'], '/');
+        } else {
+            $return['type'] = 'file';
+            $return['path'] = $content['key'];
+            $return['size'] = $content['size'];
+        }
+
+        $return['timestamp'] = strtotime($content['lastModified']);
+
+        return $return + Util::pathinfo($content['key']);
+    }
+
+    /**
+     * Extract permissions from acl
+     *
+     * @param array $acl
+     *
+     * @return array
+     */
+    protected function extractPermissions(array $acl)
+    {
+        $permissions = [];
+        foreach ($acl as $row) {
+            $ids = array_column($row['grantee'], 'id');
+            if (in_array('*', $ids)) {
+                $permissions = $row['permission'];
+                break;
+            }
+        }
+
+        return $permissions;
+    }
+}
